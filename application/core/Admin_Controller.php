e33542dfc9f4705377eb2227924e8a32e2a158ae
Luis Felipe Perez <slashcat@gmail.com>
2015-05-21 21:43:30 -0500

    add list of allowed pages when ip is blocked

--- application/core/Admin_Controller.php
+++ application/core/Admin_Controller.php
@@ -2,8 +2,9 @@
 
 class Admin_Controller extends CI_Controller {
 
-	private   $public  = array('admin/login', 'admin/blocked', 'admin/recover-password', 'admin/reset-password');
-	protected $allowed = array(0);
+	private   $public    = array('admin/login', 'admin/blocked', 'admin/lost-password', 'admin/reset-password');
+	private   $no_block  = array('admin/blocked', 'admin/lost-password', 'admin/reset-password');
+	protected $allowed   = array(0);
 
 	public function __construct()
 	{
@@ -13,14 +14,22 @@ class Admin_Controller extends CI_Controller {
 		$this->load->model('administrator');
 		# admin_auth object is initialized on administrator model
 
-		$current   = $this->uri->segment(1, '') . '/' . $this->uri->segment(2, '');
-		$is_public = in_array($current, $this->public);
+		$current         = $this->uri->segment(1, '') . '/' . $this->uri->segment(2, '');
+		$is_public       = in_array($current, $this->public);
+		$blocked_allowed = in_array($current, $this->no_block);
 
-		if( ! $is_public && !$this->admin_auth->is_logged()) {
+		if ( ! $is_public && !$this->admin_auth->is_logged()) {
 			redirect('admin/login');
 		}
 
-		$this->load->library('Template');
+		if ( $is_public && $this->admin_auth->is_logged() ) {
+			redirect('admin');
+		}
+
+		if ( $this->admin_auth->is_blocked() && !$blocked_allowed ) {
+			redirect('admin/blocked');
+		}
+
 		$this->load->library('form_validation');
 		$this->template->set_template('admin');
 		$this->form_validation->set_error_delimiters('<div class="help-block">', '</div>');
