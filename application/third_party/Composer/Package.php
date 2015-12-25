8f49ae26bb30d3b2beab67c45aabb38b62becab2
Luis Felipe Perez <slashcat@gmail.com>
2015-12-24 23:18:39 -0600

    add cleaner for google api client

--- application/third_party/Composer/Package.php
+++ application/third_party/Composer/Package.php
@@ -15,7 +15,12 @@ class Package {
 
 		if (is_dir($vendor_dir . '/codeigniter/framework/application')) {
 			exec("rm -R $vendor_dir/codeigniter/framework/application $vendor_dir/codeigniter/framework/user_guide && rm $vendor_dir/codeigniter/framework/*.*");
-			$event->getIO()->write("\n Codeigniter installation cleaned");
+			$event->getIO()->write("Codeigniter installation cleaned");
+		}
+
+		if (is_dir($vendor_dir . '/google/apiclient/examples')) {
+			exec("rm -R $vendor_dir/google/apiclient/examples $vendor_dir/google/apiclient/style $vendor_dir/google/apiclient/tests && rm $vendor_dir/google/apiclient/*.*");
+			$event->getIO()->write("Google Api Client cleaned");
 		}
 	}
 
