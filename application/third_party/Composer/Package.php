2f2b1fc4378f1f7bd557439a8bdc2333f73ea845
Luis Felipe Perez <slashcat@gmail.com>
2015-12-24 23:00:18 -0600

    change message when codeigniter installation has been cleaned

--- application/third_party/Composer/Package.php
+++ application/third_party/Composer/Package.php
@@ -15,9 +15,8 @@ class Package {
 
 		if (is_dir($vendor_dir . '/codeigniter/framework/application')) {
 			exec("rm -R $vendor_dir/codeigniter/framework/application $vendor_dir/codeigniter/framework/user_guide && rm $vendor_dir/codeigniter/framework/*.*");
+			$event->getIO()->write("\n Codeigniter installation cleaned");
 		}
-
-		$event->getIO()->write("\n Codeigniter Installed");
 	}
 
 }
