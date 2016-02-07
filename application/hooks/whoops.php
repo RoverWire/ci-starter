6272eecabc5c577906e91026bb4a06c034d09f43
Luis Felipe Perez <slashcat@gmail.com>
2016-02-07 02:50:16 -0600

    improve whoops hook to catch cli calls

--- application/hooks/whoops.php
+++ application/hooks/whoops.php
@@ -1,19 +1,26 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once('./vendor/autoload.php');
+/*
+ * composer autoload is required in order to
+ * initialize and use Whoops object.
+ */
 
+require_once('./vendor/autoload.php');
 use Whoops\Run;
-use Whoops\Handler\PrettyPageHandler;
-use Whoops\Handler\JsonResponseHandler;
 
 function load_whoops()
 {
 	$whoops = new Whoops\Run();
-	$whoops->pushHandler(new PrettyPageHandler());
+	$whoops->pushHandler(new Whoops\Handler\PrettyPageHandler());
 
+	/* checks if is an ajax call */
 	if (Whoops\Util\Misc::isAjaxRequest()) {
-		$jsonHandler = new JsonResponseHandler();
-		$run->pushHandler($jsonHandler);
+		$run->pushHandler(new Whoops\Handler\JsonResponseHandler());
+	}
+
+	/* checks if is a command line call */
+	if (Whoops\Util\Misc::isCommandLine()){
+		$run->pushHandler(new Whoops\Handler\PlainTextHandler());
 	}
 
 	$whoops->register();
