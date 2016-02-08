568c02783978974c9150705e51b9d4cfa12c329e
Luis Felipe Perez <slashcat@gmail.com>
2016-02-07 23:48:34 -0600

    offline styles fix

--- application/views/layouts/admin.php
+++ application/views/layouts/admin.php
@@ -8,7 +8,8 @@
   <link rel="stylesheet" href="/assets/vendor/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="/assets/css/admin.css">
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="/assets/vendor/offline/themes/offline-language-spanish.css">
+  <link rel="stylesheet" href="/assets/vendor/offline/themes/offline-theme-default.css">
+  <link rel="stylesheet" href="/assets/vendor/offline/themes/offline-theme-spanish.css">
 
   <?php echo $_styles ?>
 
